e482e9ab17aec99c89f2121617c6d4e3b4932ef2
SacDeNoeuds <92589074+SacDeNoeuds@users.noreply.github.com>
2025-04-26 11:04:42 +0200

    feat(cli): use cac over catacli

--- src/cli.ts
+++ src/cli.ts
@@ -1,47 +1,55 @@
 #!/usr/bin/env node
+import { cac } from 'cac'
+import { glob } from "glob"
+import { readFileSync } from 'node:fs'
+import * as path from "node:path"
+import { fileURLToPath } from 'node:url'
+import { generate } from "./app.js"
 
-import { makeCommand, makeStringFlag, mergeFlag } from "catacli"
-import * as glob from "glob"
-import * as path from "path"
-import { generate } from "./app"
+const currentFilePath = fileURLToPath(import.meta.url)
+const packageJsonPath = path.resolve(path.dirname(currentFilePath), '../package.json')
+const pkg = JSON.parse(readFileSync(packageJsonPath, 'utf-8'))
 
-const flag = mergeFlag(
-  makeStringFlag("filepath", {
-    usage: "src file path (only single file accepted)",
-  }),
-  makeStringFlag("fileMatch", {
-    usage: "src file path (regexp).",
-  }),
-)
+const cli = cac(pkg.name)
+// gen-jet
 
-const command = makeCommand({
-  name: "tsdoc-testify",
-  description: "documentation testing generator for tsdoc",
-  version: "0.0.1",
-  usage: "tsdoc-testify [OPTIONS]",
-  flag,
-  handler: async (_, params) => {
-    if (!params.filepath.value && !params.fileMatch.value) {
-      console.log("`--filepath` or `--fileMatch` is required.")
+cli
+  .version(pkg.version)
+  .command('[pattern]', 'Generate tests from JSDoc @example')
+  .usage("npx gen-jet './src/**/*.ts'")
+  .option(
+    '--test-function-name [name]',
+    'Name of test function (default: "test")',
+    { default: 'test' }
+  )
+  .option(
+    '--test-file-extension [ext]',
+    'Test file extension (default: ".example.test.ts")',
+    { default: '.example.test.ts' }
+  )
+  .option(
+    '--header [text]',
+    'Header text to include in test files, ie: `import { test } from "vitest"`',
+    { default: '' }
+  )
+  .action(async (pattern, options) => {
+    if (!pattern) {
+      console.error('Error: Missing required glob pattern')
+      cli.outputHelp()
+      process.exit(1)
     }
-
-    if (params.fileMatch.value) {
-      try {
-        const files = await glob.glob(params.fileMatch.value)
-        await Promise.all(files.map(async (f) => {
-          const filePath = path.resolve(process.cwd(), f)
-          await generate(filePath)
-        }))
-      } catch (error) {
-        console.error(error)
-      }
+    console.debug('Generating tests with:')
+    console.debug({ pattern, options })
+    try {
+      const unfilteredFiles = await glob(pattern)
+      const files = unfilteredFiles.filter((fileName) => !fileName.endsWith(options.testFileExtension))
+      await Promise.all(files.map(async (f) => {
+        const filePath = path.resolve(process.cwd(), f)
+        await generate(filePath, options)
+      }))
+    } catch (error) {
+      console.error(error)
     }
+  })
 
-    if (params.filepath.value) {
-      const filePath = path.resolve(process.cwd(), params.filepath.value)
-      await generate(filePath)
-    }
-  },
-})
-
-command(process.argv.splice(2))
+cli.parse()
