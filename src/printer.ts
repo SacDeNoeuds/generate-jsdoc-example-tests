15a24b888d4ca7185878c3de8056a124a1719dce
SacDeNoeuds <92589074+SacDeNoeuds@users.noreply.github.com>
2025-04-26 10:08:56 +0200

    feat(printer): if installed, resolve prettier project config

--- src/printer.ts
+++ src/printer.ts
@@ -1,27 +1,48 @@
-import { format } from "prettier"
+import * as path from 'node:path'
+import * as prettier from "prettier"
 import * as ts from "typescript"
-import type { GenerateOptions } from './app'
+import type { GenerateOptions } from "./app"
 
-export async function print(ast: ts.Node, options: Pick<GenerateOptions, 'testFunctionName'>): Promise<string> {
+export async function print(
+  ast: ts.Node,
+  options: Pick<GenerateOptions, "testFunctionName">,
+): Promise<string> {
   const resultFile = ts.createSourceFile(
     "result.ts",
     "",
     ts.ScriptTarget.Latest,
     false,
     ts.ScriptKind.TS,
-  )
+  );
   const printer = ts.createPrinter({
     newLine: ts.NewLineKind.LineFeed,
     omitTrailingSemicolon: true,
-  })
+  });
   const result = printer
     .printNode(ts.EmitHint.Unspecified, ast, resultFile)
-    .replaceAll(`\n${options.testFunctionName}(`, `\n\n${options.testFunctionName}(`)
+    .replaceAll(
+      `\n${options.testFunctionName}(`,
+      `\n\n${options.testFunctionName}(`,
+    );
 
-  const formatted = await format(result, {
+  const prettierConfig = await resolvePrettierConfig();
+  const formatted = await prettier.format(result, {
     parser: "typescript",
-    semi: false,
-    singleQuote: true,
-  })
-  return formatted
+    ...prettierConfig,
+  });
+  return formatted;
+}
+
+async function resolvePrettierConfig(): Promise<prettier.Options> {
+  // NOTE: I am using readme instead of just `process.cwd()` because
+  // `prettier.resolveConfig` inspects the directory of a file, not the directory directly.
+  const pathOfReadme = path.resolve(process.cwd(), 'README.md')
+  const maybeConfig = await prettier.resolveConfig(pathOfReadme);
+
+  return (
+    maybeConfig ?? {
+      semi: false,
+      singleQuote: true,
+    }
+  );
 }
